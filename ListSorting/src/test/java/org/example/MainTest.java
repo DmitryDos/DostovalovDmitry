dd9d6a3340acf5caa207d1110e7ff75283bf1614
DmitryDos <dostovalov.ds@phystech.edu>
2023-09-30 21:39:53 +0300

    Create ProjectBase for SortingMethods, set jacoco

---
+++ ListSorting/src/test/java/org/example/MainTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class MainTest {
+    @Test
+    void testSum() {
+        assertEquals(2, 1 + 1);
+    }
+}
